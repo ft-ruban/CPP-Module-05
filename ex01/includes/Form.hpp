945ecfd83d1058b8a4529d5486f8dd3378c652e8
Lea Devoudelle-Royer <ldevoude@z3r8p3.42lyon.fr>
2026-02-05 11:09:13 +0100

    added a distinct character _ at the end of the name of private variables for better readability

---
+++ ex01/includes/Form.hpp
@@ -0,0 +1,18 @@
+
+
+//#include <iostream>
+#include <string>
+
+class Form{
+    public:
+    //constructor
+    //cpy constructor
+    //override =
+    //destructor
+
+    private:
+        const std::string name_;
+        bool  is_signed_;
+        const int req_grade_to_sign_;
+        const int req_grade_to_exec_;
+};
---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <ldevoude@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 15:10:09 by ldevoude          #+#    #+#             */
+/*   Updated: 2026/02/05 09:38:52 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+int main(void){
+    Bureaucrat* wallace = NULL;
+
+    try
+    {
+        wallace = new Bureaucrat("Wallace Breen", 150);
+        std::cout<<*wallace<<std::endl;
+        wallace->increment();
+        std::cout<<*wallace<<std::endl;
+        wallace->decrement();
+        wallace->decrement();
+        std::cout<<*wallace<<std::endl;
+        std::cout<<"end of try scope"<<std::endl;
+    }
+    catch (std::exception & e)
+    {
+        delete wallace;
+        std::cout<<"exception caught"<<std::endl;
+        return(1);
+    }
+    std::cout<<"no exception caught"<<std::endl;
+    //std::cout<<wallace->getName()<<wallace->getGrade()<<std::endl;
+    delete wallace;
+    return 0; 
+}
+
