6a8a23338da20daaca794995fe0acc3f1ebef069
Lea Devoudelle-Royer <ldevoude@z3r9p1.42lyon.fr>
2026-02-09 07:09:25 +0100

    added files for ex02

---
+++ ex02/includes/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <ldevoude@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 11:35:45 by ldevoude          #+#    #+#             */
+/*   Updated: 2026/02/06 15:04:48 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat{
+    public:
+        class GradeTooHighException : public std::exception{
+            public:
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception{
+            public:
+                virtual const char* what() const throw();
+        };
+        Bureaucrat(std::string given_name, int given_grade);
+        Bureaucrat(const Bureaucrat &other);
+        Bureaucrat &operator=(const Bureaucrat &other);
+        ~Bureaucrat();
+
+        std::string getName()const;
+        int         getGrade()const;
+        
+        void increment();
+        void decrement();
+        void signForm(Form &form);
+    
+    private:
+        const std::string name_;
+        int grade_;
+};
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ ex02/includes/Form.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <ldevoude@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 15:05:10 by ldevoude          #+#    #+#             */
+/*   Updated: 2026/02/06 15:05:34 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form{
+    public:
+        class GradeTooHighException : public std::exception{
+            public:
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception{
+            public:
+                virtual const char* what() const throw();
+        };
+        Form(std::string given_name, int given_req_grade_to_sign, int given_req_grade_to_exec);
+        Form(const Form &other);
+        Form &operator=(const Form &other);
+        ~Form();
+
+        std::string getName()const;
+        bool        getIsSigned()const;
+        int         getReqGradeToSign()const;
+        int         getReqGradeToExec()const;
+    
+        void beSigned(const Bureaucrat &other);
+
+    private:
+        const std::string name_;
+        const int         req_grade_to_sign_;
+        const int         req_grade_to_exec_;
+        bool              is_signed_;
+
+};
+std::ostream &operator<<(std::ostream &out, const Form &form);
+
+#endif
